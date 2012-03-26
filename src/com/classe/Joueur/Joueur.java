0fee10944f15938f3ef556026ce0e5221d1e7747
johann <dubois.johann@gmail.com>
2012-03-26 13:55:49 +0200

    c

--- src/com/classe/Joueur/Joueur.java
+++ src/com/classe/Joueur/Joueur.java
@@ -3,8 +3,6 @@ package com.classe.Joueur;
 public class Joueur {
 	private int Id;
 	private String Nom;
-	private int Level;
-	private float Money;
 	
 	public int getId() {
 		return Id;
@@ -17,18 +15,6 @@ public class Joueur {
 	}
 	public void setNom(String nom) {
 		Nom = nom;
-	}
-	public int getLevel() {
-		return Level;
-	}
-	public void setLevel(int level) {
-		Level = level;
-	}
-	public float getMoney() {
-		return Money;
-	}
-	public void setMoney(float money) {
-		Money = money;
 	}
 	
 }
